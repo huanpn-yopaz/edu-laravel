8eba02f2c0b768e9a34847f3b2b3994204c3267f
baitapnhom12 <phuan3366@gmail.com>
2024-01-08 18:00:25 +0700

    up

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('users', UserController::class);
-Route::resource('post', PostController::class);
-Route::resource('zoom', ZoomController::class);
-Route::resource('object', ObjectsController::class);
